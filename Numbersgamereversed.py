1a62770b18ab4fdfd84a568a1356e0e0eaf77284
MaxB3435 <bortasmax@gmail.com>
2024-02-14 15:53:18 +0100

    Update Numbersgamereversed.py

--- Numbersgamereversed.py
+++ Numbersgamereversed.py
@@ -3,7 +3,7 @@ ng = 1
 
 h = 64
 l = 1
-g = random.randint(l,h)
+g = h/2
 
 #print ('Välj ett nummer')
 #number = int(input())
@@ -16,11 +16,11 @@ while True:
         if g > number:
             print('Nix, lägre')
             h = g - 1
-            g = random.randint(l,h)
+            g = int((h+l)/2)
         elif g < number:
             l = g + 1
             print('Nix, högre')
-            g = random.randint(l,h)
+            g = int((h+l)/2)
     else:
          print('Jag klarade spelet på', ng,'gissningar!')
          exit()
